d4c45d083236b3b40fe47312cf8a8c2e0f9b014f
lashatatkhashvili <59742532+lashatatkhashvili@users.noreply.github.com>
2020-09-18 19:59:45 +0400

    Updates

---
+++ precache-manifest.028baef95a762819aeb5b899163a98da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d551add5029511b5e5f466088ba4906",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "834c72338652d919ad46",
+    "url": "/calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c10ffce60bae3f7d579",
+    "url": "/calculator/static/js/2.f4157476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.f4157476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "834c72338652d919ad46",
+    "url": "/calculator/static/js/main.d7834d65.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
